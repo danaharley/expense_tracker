2a881fceba1796343685e215e611939682864f98
danaharley <danaharley.github@gmail.com>
2020-12-29 23:29:37 +0700

    Penambahan Speechly

--- src/components/Detail/Details.js
+++ src/components/Detail/Details.js
@@ -3,15 +3,18 @@ import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
 import { Doughnut } from "react-chartjs-2";
 
 import useStyles from "./styles";
+import useTransactions from "../../useTransactions";
 
-const Details = ({ title }) => {
+const Details = ({ title, subheader }) => {
+  const { total, chartData } = useTransactions(title);
   const classes = useStyles();
+
   return (
     <Card className={title === "Income" ? classes.income : classes.expense}>
-      <CardHeader title={title} />
+      <CardHeader title={title} subheader={subheader} />
       <CardContent>
-        <Typography variant="h5">$50</Typography>
-        {/* <Doughnut data="DATA" /> */}
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
       </CardContent>
     </Card>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { SpeechProvider } from "@speechly/react-client";
 
 import { Provider } from "./context/context";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  <SpeechProvider appId="eea7ca75-5df9-457c-b356-7b4b8af43e49" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById("root")
 );
